21d9c7d95f88e6e6e64bea79e26f830726c97338
rcarlosdev <carl.rest19@gmail.com>
2025-09-13 16:21:05 -0500

    feat: :sparkles: Inicio de session, correo y contraseña y con google

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  children: React.ReactNode
+}
+
+function layout({ children }: Props) {
+  return (
+    <div className="bg-muted flex min-h-screen items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default layout
