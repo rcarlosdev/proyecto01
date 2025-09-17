8204e848d626429b8b7d1ac1347c06070c7cc629
rcarlosdev <carl.rest19@gmail.com>
2025-09-16 20:59:54 -0500

    refactor: actualizar configuración de conexión a la base de datos y dependencias

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+})
