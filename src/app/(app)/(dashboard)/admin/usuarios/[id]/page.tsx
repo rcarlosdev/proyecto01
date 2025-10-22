aedc540d2d953ed544a17628af2fb406fce5ada9
rcarlosdev <carl.rest19@gmail.com>
2025-10-22 09:36:02 -0500

    Agregar interfaz PageProps para mejorar la tipificación en la página de detalles de usuario

--- src/app/(app)/(dashboard)/admin/usuarios/[id]/page.tsx
+++ src/app/(app)/(dashboard)/admin/usuarios/[id]/page.tsx
@@ -1,4 +1,14 @@
 import UsuarioDetailView from "@/modules/usuarios/ui/views/usuario-detail-view";
+import { int } from "zod";
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+  searchParams: {
+    tab?: string;
+  };
+}
 
 export default function Page({ 
   params,
